2d14e59380a889749c8c85246e7da210cabaf251
natleo <n8wyliu@gmail.com>
2023-08-25 15:53:33 -0700

    start over on site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import React from 'react';
-import landscape from './assets/landscape1.jpg';
-import bicycle from './assets/bicycle.jpg';
-import fields from './assets/fields.jpg';
-import flowers from './assets/flowers.jpg';
-import './App.css';
-import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <header className="App-header">
-        <img src={landscape} height="500" width="300"/>
-        <img src={bicycle} height="400" width="500"/>
-        <img src={fields} height="350" width="500"/>
-        <img src={flowers} height="400" width="500"/>
-      </header>
-    </div>
-  );
+  return (<div>
+    <h1>Nat Liu</h1>
+    <h3>Computer Engineer</h3>
+  </div>);
 }
 
 export default App;
